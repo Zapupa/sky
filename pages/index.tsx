9762a71a4900792651f62acefb54432f61423a16
Zapupa <vitaliy2005rus93@gmail.com>
2024-04-10 13:21:54 +0500

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter, Roboto, Montserrat, Epilogue } from "next/font/google";
+import styles from "@/styles/Home.module.scss";
+const epilogue = Epilogue({ subsets: ["latin"] });
+
+
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+
+    </div>
+  );
+}
